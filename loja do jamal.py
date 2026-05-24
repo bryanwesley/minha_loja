0faf1e5d3bc26f9ef5693337ddaafaaf6c6101d7
bryan <bryansviana@hotmail.com>
2026-05-24 13:03:48 -0300

    adicionei as propriedades produtos e clientes

--- loja do jamal.py
+++ loja do jamal.py
@@ -1,11 +1,12 @@
 class loja:
-    def __init__(self, nome, funcionarios, chefe):
+    def __init__(self, nome, funcionarios, produtos, clientes, chefe):
        self.nome = nome
        self.funcionarios = funcionarios
+       self.produtos = produtos
+       self.clientes = clientes
        self.chefe = chefe
-loja_do_jamal = loja('lojadojamal', "A, B, C, D, E, F, G","bryanwsley" )
+loja_do_jamal = loja('lojadojamal', "A, B, C, D, E, F, G", "calças, blusas", 1200, "bryanwesley" )
 print(loja_do_jamal)
 print(loja_do_jamal.nome)
 print(loja_do_jamal.funcionarios)
 print(loja_do_jamal.chefe)
-
